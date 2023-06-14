9369801307463208624e8173cf6b06548f08754f
Blueflymedia <paul.blueflymedia@gmail.com>
2023-06-13 20:24:00 -0400

    fixed display issues

--- src/App.js
+++ src/App.js
@@ -10,10 +10,13 @@ import {
 import { ethers } from "ethers";
 import { useEffect, useState } from "react";
 import "./styles/Home.css";
-import { stakingContractAddress } from "../src/caAddresses";
+// import { stakingContractAddress } from "../src/caAddresses";
 import Logo from "./images/logo-2x.png";
+// api key for thirdweb 02f2d6f5bdafebb308ee17e411b4e58903375c2ef3778edac28d6f993227dff0777e44c9d527bedc8e49244b41bd542394a89f9e554b12cebd86e37419ca6a50
+// ethereum.rpc.thirdweb.com
+// https://ipfs.thirdwebcdn.com/ipfs/
 
-export default function Home() {
+export default function App() {
   function decimalRound(bal, decimals) {
     if (!decimals) decimals = 2;
     return Number.parseFloat(bal).toFixed(decimals);
@@ -22,16 +25,19 @@ export default function Home() {
   function truncateNumber(bal) {
     return bal.toFixed(2);
   }
-
+  const contractAddress = "0x0777E4C556b76143349b81D86EAc8D36b7efB58E";
+  const yahToken = "0x421a312D1C443faA673AE47338c0c42Fb40CdfD0";
   const address = useAddress();
+  // const signer = ethers.getDefaultProvider();
   const [amountToStake, setAmountToStake] = useState(0);
   const [amountToWithdraw, setAmountToWithdraw] = useState(0);
   const [amountToClaim, setAmountToClaim] = useState(0);
-  // const rewardTokenAddress = "0x421a312D1C443faA673AE47338c0c42Fb40CdfD0";
+  const [ethRewards, setEthRewards] = useState(0);
+
 
   // Initialize all the contracts
   const { contract: staking, isLoading: isStakingLoading } = useContract(
-    stakingContractAddress,
+    contractAddress,
     "custom"
   );
   // const { data, isLoading, error } = useTokenDecimals(contract);
@@ -52,22 +58,40 @@ console.log('reward token',rewardTokenAddress);
     useContract(rewardTokenAddress, "token");
 
   // Token balances
-  const { data: stakingTokenBalance, refetch: refetchStakingTokenBalance } =
-    useTokenBalance(stakingToken, address);
-  const { data: rewardTokenBalance, refetch: refetchRewardTokenBalance } =
-    useTokenBalance(rewardsToken, address);
-    console.log('staking token balance:',stakingTokenBalance);
-    console.log('reward token balance:',rewardTokenBalance);
+  const { 
+    data: unstakedTokenBalance, 
+    refetch: refetchUnstakedTokenBalance,
+    error: unstakedTokenBalanceError } =
+    useTokenBalance(
+      rewardsToken, 
+      address
+      );
 
-    // const { data: stakedYah, isLoading } = useContractRead(contract, "balanceOf", [{address: address || "0"}]);
+  const { 
+    data: stakedTokenBalance, 
+    refetch: refetchStakedTokenBalance,
+    error: stakedTokenBalanceError } = useContractRead(
+      staking, 
+      "balanceOf", 
+      ([address]),
+      );
+
+  const { data: rewardsTokenBalance, refetch: refetchRewardsBalance } =
+  useContractRead(staking, "rewards", [address]);
+
+    //check to see if token balances are working
+    console.log('unstaked token balance:',unstakedTokenBalance);
+    console.log('staked token balance:',stakedTokenBalance);
+
+  //  const { data, isLoading, error } = useContractRead(staking, "balanceOf", [address || "0"]);
     
   // Get staking data
-  const {
-    data: stakeInfo,
-    refetch: refetchStakingInfo,
-    isLoading: isStakeInfoLoading,
-  } = useContractRead(staking, "balanceOf", [address]);
-  console.log('staked yah:',stakeInfo);
+  // const {
+  //   data: stakeInfo,
+  //   refetch: refetchStakingInfo,
+  //   isLoading: isStakeInfoLoading,
+  // } = useContractRead(staking, "balanceOf", address);
+  // console.log('staked yah:',stakeInfo);
   
   useEffect(() => {
     setInterval(() => {
@@ -76,9 +100,9 @@ console.log('reward token',rewardTokenAddress);
   }, []);
 
   const refetchData = () => {
-    refetchRewardTokenBalance();
-    refetchStakingTokenBalance();
-    refetchStakingInfo();
+    refetchStakedTokenBalance();
+    refetchUnstakedTokenBalance();
+    refetchRewardsBalance();
   };
 
   return (
@@ -86,7 +110,7 @@ console.log('reward token',rewardTokenAddress);
     <header>
       <div className="container">
         <div className="row pt-2">
-        <div class="d-flex justify-content-end">
+        <div className="d-flex justify-content-end">
           <div className="connect">
           <ConnectWallet theme="dark" dropdownPosition={{ side: 'bottom', align: 'center'}} />
         </div>
@@ -110,13 +134,17 @@ console.log('reward token',rewardTokenAddress);
         <div className="col-6 col-md-3 g-0">
         <div className="card-display">
             <h3>Unstaked YAH</h3>
-            <h4>{decimalRound(stakingTokenBalance?.displayValue)}</h4>
+            <h4>
+            {decimalRound(unstakedTokenBalance?.displayValue)}
+              {/* {unstakedTokenBalance && (decimalRound(ethers.utils.formatUnits(unstakedTokenBalance,9)))} */}
+              </h4>
           </div>
           </div>
           <div className="col-6 col-md-3 g-0">
           <div className="card-display">
             <h3>Staked YAH</h3>
-            {/* <h4>0.0{stakeInfo && ethers.utils.formatUnits(stakeInfo[0],9)}</h4> */}
+            <h4>{stakedTokenBalance && (decimalRound(ethers.utils.formatUnits(stakedTokenBalance,9)))}
+              </h4>
             
           </div>
           </div>
@@ -129,7 +157,7 @@ console.log('reward token',rewardTokenAddress);
           <div className="col-6 col-md-3 g-0">
           <div className="card-display">
             <h3>YAH Rewards</h3>
-            {/* <h4>0.0{stakeInfo && ethers.utils.formatUnits(stakeInfo[1],9)}</h4> */}
+            {/* {rewardsTokenBalance && (decimalRound(ethers.utils.formatUnits(rewardsTokenBalance,9)))} */}
           </div>
           </div>
         </div>
@@ -137,7 +165,7 @@ console.log('reward token',rewardTokenAddress);
     <div className="container pt-5 pb-5">
       <div className="row text-center">
         <div className="col-12 col-md-6">
-            <div class="balance-form-area">
+            <div className="balance-form-area">
               <h4 className="labels">Stake YAH</h4>
               {/* <h5></h5> */}
               <input 
@@ -155,6 +183,7 @@ console.log('reward token',rewardTokenAddress);
       action={(contract) => {
         contract.call("stake", [ethers.utils.parseUnits(amountToStake,9)])
       }}
+      onSuccess={(result) => alert("Staking Transaction successful")}
     >
       stake
     </Web3Button>
@@ -167,7 +196,7 @@ console.log('reward token',rewardTokenAddress);
         </div>
 
         <div className="col-12 col-md-6">
-            <div class="balance-form-area">
+            <div className="balance-form-area">
               <h4 className="labels">Unstake YAH</h4>
               <input 
               className="input-staking" 
@@ -202,7 +231,7 @@ console.log('reward token',rewardTokenAddress);
             <h2 className="title2 pt-5">
                 We are all here.
               </h2>
-              <p className="description">more to be revealed. The light shines our path and illuminates the mind in its own time.
+              <p className="description">only those who stake will be eligible for ETH rewards from partner projects, future airdrops, alpha access and more as we grow. What makes this different is your ETH rewards do not come from taxes, so volume is not necessarily required to earn.
               </p>
           </div>
         </div>
